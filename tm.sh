e2c734874b43c2aea83da7771feccf9b03d5dfe6
dexter <178529795@qq.com>
2023-01-08 11:50:05 +0800

    update

--- tm.sh
+++ tm.sh
@@ -1,5 +1,4 @@
-read -p "Enter the dir name: " dir
-cd plugin-${dir}
+cd monibuca
 latest=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo $latest
 read -p "Enter the tag name: " tag
