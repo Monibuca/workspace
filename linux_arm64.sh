14da7028f4be81e8ba674760d26ec8542751726e
langhuihui <178529795@qq.com>
2023-05-17 09:31:51 +0800

    update

---
+++ linux_arm64.sh
@@ -0,0 +1,4 @@
+cd monibuca
+export GOOS=linux
+export ARCH=arm64
+go1.19 build -o monibuca_linux_arm64
--- m1.sh
+++ m1.sh
@@ -1,3 +1,3 @@
 cd monibuca
-export ARCH=amd64
+export ARCH=arm64
 go1.19 build -o monibuca_arm64
